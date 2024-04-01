80f360298f76a07a359362e18d3d2a5a52f0bea9
Ryan Krebs <ryankrebs016@gmail.com>
2024-04-01 11:14:07 -0400

    update the script too

--- src/ice-config.c
+++ src/ice-config.c
@@ -170,7 +170,7 @@ int init_acq_config(acq_config_t * cfg)
   SECT.RF[0].mask =  0x092000; //upward pointing LPDAs
   SECT.RF[0].window = 50 ; // ?!?? 
   SECT.RF[0].num_coincidences = 2; 
-  SECT.RF[0].readout_delay=30; //delay 30*(53.3ns)=1600ns
+  SECT.RF[0].readout_delay=19; //delay 19*(53.3ns)=1013ns
   SECT.RF[0].readout_delay_mask=0b1011; //delay all power and helper strings. not surface
 
   //Downward Surface
