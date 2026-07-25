17c5bbdfbf06db4a113c4f1abef5d966f02976f6
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2026-07-25 18:34:47 +0200

    fix

--- src/rno-g-acq.c
+++ src/rno-g-acq.c
@@ -455,7 +455,7 @@ static int didaq_configure()
       ds->didaq_phased_trigger_thresholds[i] = cfg.didaq.thresholds.phased.initial[i];
       ds->didaq_phased_servo_thresholds[i] = clamp(
         cfg.didaq.thresholds.phased.initial[i] * cfg.didaq.servo.phased_servo_thresh_frac +
-        cfg.didaq.servo.servo_thresh_offsetm, 0, 65535);
+        cfg.didaq.servo.servo_thresh_offset, 0, 65535);
     }
   }
 
