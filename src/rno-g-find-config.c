88e569a094fb7109abdd8f535be92aa84568e90f
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2026-08-18 11:01:03 +0200

    WS and fix pedentic warning

--- src/rno-g-find-config.c
+++ src/rno-g-find-config.c
@@ -1,42 +1,40 @@
-#include "ice-common.h" 
+#include "ice-common.h"
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 
-int main(int nargs, char ** args) 
+int main(int nargs, char ** args)
 {
-  char * cfgpath = NULL; 
-  char * cfgname = "acq.cfg"; 
+  char * cfgpath = NULL;
+  char * cfgname = "acq.cfg";
 
-  if (nargs > 1) cfgname = args[1]; 
-  else printf("No config name passed, assuming acq.cfg\n"); 
-  if (nargs > 2) 
+  if (nargs > 1) cfgname = args[1];
+  else printf("No config name passed, assuming acq.cfg\n");
+  if (nargs > 2)
   {
-    cfgpath = args[2]; 
-    printf("Using provided cfgpath: %s\n", cfgpath); 
+    cfgpath = args[2];
+    printf("Using provided cfgpath: %s\n", cfgpath);
   }
 
   char * found = 0;
-  char * renamed = 0;  
-  FILE * f = find_config(cfgname, cfgpath, &found, &renamed); 
-  if (!f) 
+  char * renamed = 0;
+  FILE * f = find_config(cfgname, cfgpath, &found, &renamed);
+  if (!f)
   {
-    printf("Not found!\n"); 
-    return 1; 
+    printf("Not found!\n");
+    return 1;
 
   }
-  printf("Found: %s, renamed? %s\n", found, renamed?: "no"); 
-  char link[1024], path[1024]; 
-  int fd = fileno(f); 
-  sprintf(path,"/proc/self/fd/%d", fd); 
-  readlink(path, link, sizeof(link)-1); 
-  printf("fd: %d, linkname: %s\n", fd, link); 
-  fclose(f); 
-  return 0; 
+  printf("Found: %s, renamed? %s\n", found, renamed? "yes" : "no");
+  char link[1024], path[1024];
+  int fd = fileno(f);
+  sprintf(path,"/proc/self/fd/%d", fd);
+  readlink(path, link, sizeof(link)-1);
+  printf("fd: %d, linkname: %s\n", fd, link);
+  fclose(f);
+  return 0;
 
 
 
 
 }
-
-
